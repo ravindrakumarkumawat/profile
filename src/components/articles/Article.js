a371743b47ca6800059290bda589a0529c3ae99b
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-08 22:34:40 +0530

    work is commpleted

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -1,34 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Article.css";
 
 const Article = () => {
   return (
-    
-    <div className="profile-details-works">
-    <h4>Notable works</h4>
-    <p>Based on the popularity of articles</p>
-    <div className="article-container"> 
-      <div className="article-details">
-        <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" alt="article-img"/>
-        <p>Nature</p>
-        <h5>Let us plant more trees from this year</h5>
-      </div>
+    <div className="article">
+      <h4>Notable works</h4>
+      <p>Based on the popularity of articles</p>
+      <div className="article-slider">
+        <div className="article-details">
+          <img
+            src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png"
+            alt="article-img"
+          />
+          <p>Nature</p>
+          <h5>Let us plant more trees from this year</h5>
+        </div>
 
-      <div className="article-details">
-        <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" alt="article-img"/>
-        <p>Nature</p>
-        <h5>Let us plant more trees from this year</h5>
-      </div>
+        <div className="article-details">
+          <img
+            src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png"
+            alt="article-img"
+          />
+          <p>Travel</p>
+          <h5>Let us plant more trees from this year</h5>
+        </div>
 
-      <div className="article-details">
-        <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" alt="article-img"/>
-        <p>Nature</p>
-        <h5>Let us plant more trees from this year</h5>
-      </div>           
+        <div className="article-details">
+          <img
+            src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png"
+            alt="article-img"
+          />
+          <p>Technology</p>
+          <h5>Let us plant more trees from this year</h5>
+        </div>
+      </div>
     </div>
+  );
+};
 
-      
-  </div> 
-  )
-}
-
-export default Article
+export default Article;
--- src/components/profileCard/ProfileCard.js
+++ src/components/profileCard/ProfileCard.js
@@ -3,22 +3,26 @@ import "./ProfileCard.css";
 import UserInfo from "../userinfo/UserInfo";
 import Achievement from "../achievements/Achievement";
 import Buttons from "../buttons/Buttons";
+import Article from "../articles/Article";
 
 const ProfileCard = () => {
   return (
-    <div className="profile-container">
-      <div className="profile-card">
-        <img
-          src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png"
-          alt="card-img"
-        />
-        <div className="profile-card-info">
-          <UserInfo />
-          <Achievement />
+    <>
+      <div className="profile-container">
+        <div className="profile-card">
+          <img
+            src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png"
+            alt="card-img"
+          />
+          <div className="profile-card-info">
+            <UserInfo />
+            <Achievement />
+          </div>
         </div>
-      </div>      
-      <Buttons />
-    </div>
+        <Buttons />
+      </div>
+      <Article />
+    </>
   );
 };
 
