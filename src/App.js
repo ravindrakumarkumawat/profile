4e86ab5deffb2d56e6b9724183094b9367270fdf
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-08 19:03:22 +0530

    style is left

--- src/App.js
+++ src/App.js
@@ -37,8 +37,39 @@ function App() {
           </div>
 
         </div>
-        <div className="profile-details-works"></div> 
-        <div className="profile-details-featured-article"></div>
+        <div className="profile-details-works">
+          <h4>Notable works</h4>
+          <p>Based on the popularity of articles</p>
+          <div className="article-container"> 
+            <div className="article-details">
+              <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" />
+              <p>Nature</p>
+              <h5>Let us plant more trees from this year</h5>
+            </div>
+
+            <div className="article-details">
+              <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" />
+              <p>Nature</p>
+              <h5>Let us plant more trees from this year</h5>
+            </div>
+
+            <div className="article-details">
+              <img src="https://res.cloudinary.com/diqcnmefm/image/upload/v1607430663/images/stevejobs_butuw4.png" />
+              <p>Nature</p>
+              <h5>Let us plant more trees from this year</h5>
+            </div>           
+          </div>
+
+            
+        </div> 
+
+        <div className="profile-details-featured-article">
+          <div> 
+            <h6>Featured Articles</h6>
+          </div>        
+          <h4>Natural mood regulation low or even absent in people with depression</h4>
+        </div>
+
       </div>
 
       <div className="Profile-settings">
